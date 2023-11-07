2771ba418c2cbf0f73652559c43c7654dc0e54a8
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-11-07 11:51:36 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// css
+import "./styles/index.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>
+        🌠 Hello{" "}
+        <span style={{ color: "var(--ut-orange)" }}>AllGoodsGalaxy!</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
