f14aad6214a37faa2b76cf4689473d1242871a67
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-11-19 11:30:36 -0300

    feat: Redirect unauthenticated users to login page on product actions click

--- src/components/ProductActions.jsx
+++ src/components/ProductActions.jsx
@@ -1,10 +1,37 @@
+// dependencies
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// contexts
+import { UserContext } from "../contexts/userContext";
+
 export default function ProductActions() {
+  const { userAccount, setUserAccount } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  function handleHeartClick() {
+    if (!userAccount.logged) {
+      navigate("/login");
+    } else {
+      console.log("clicked!");
+    }
+  }
+
+  function handleAddToCartClick() {
+    if (!userAccount.logged) {
+      navigate("/login");
+    } else {
+      console.log("clicked!");
+    }
+  }
+
   return (
     <div className="actions">
-      <a href="">
+      <a onClick={handleHeartClick}>
         <i className="fa fa-heart"></i>
       </a>
-      <a href="">
+
+      <a onClick={handleAddToCartClick}>
         <i className="fa fa-cart-plus"></i>
       </a>
     </div>
