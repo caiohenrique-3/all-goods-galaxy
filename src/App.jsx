bfff8b432a6ea663972b894a37b27ea536d009a0
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-11-20 15:49:50 -0300

    feat: Add check for localStorage availability

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // dependencies
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // components
 import Header from "./components/Header";
@@ -10,6 +10,20 @@ import MainContent from "./components/MainContent";
 import "./styles/index.css";
 
 function App() {
+  const [localStorageEnabled, setLocalStorageEnabled] = useState(true);
+
+  useEffect(() => {
+    try {
+      // Try to read from and write to localStorage
+      localStorage.setItem("localStorageTest", "test");
+      localStorage.getItem("localStorageTest");
+      localStorage.removeItem("localStorageTest");
+    } catch (error) {
+      // If an error occurs, set localStorageEnabled to false
+      setLocalStorageEnabled(false);
+    }
+  }, []);
+
   // When the user gets redirected back here,
   // scroll back to the top of the page.
   useEffect(() => {
@@ -19,6 +33,12 @@ function App() {
   return (
     <div className="container">
       <Header />
+      {!localStorageEnabled && (
+        <h4 style={{ color: "red", padding: "1rem", textAlign: "center" }}>
+          LocalStorage is not enabled in your browser. Some functionalities will
+          not work.
+        </h4>
+      )}
       <HeroSwiper />
       <MainContent />
     </div>
